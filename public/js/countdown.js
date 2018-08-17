e7edb8366d011717a4844747cb513b93d3354295
Jeff Cameron <jcameronjeff@gmail.com>
2018-08-16 23:04:50 -0400

    new

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -1,29 +1,42 @@
 var countDownDate = new Date("Oct 14, 2018 17:00:00").getTime();
+var counter = document.getElementById("days");
 
-// Update the count down every 1 second
-var x = setInterval(function () {
+if (counter) {
+    // Update the count down every 1 second
+    var x = setInterval(function () {
 
-    // Get todays date and time
-    var now = new Date().getTime();
+        // Get todays date and time
+        var now = new Date().getTime();
 
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="demo"
-    document.getElementById("days").innerHTML = "<p>" + days + " days</p>";
-    document.getElementById("hours").innerHTML = "<p>" + hours + " hours</p>";
-    document.getElementById("minutes").innerHTML = "<p>" + minutes + " minutes</p>";
-    document.getElementById("seconds").innerHTML = "<p>" + seconds + " seconds</p>";
+        // Display the result in the element with id="demo"
+        document
+            .getElementById("days")
+            .innerHTML = "<p>" + days + " days</p>";
+        document
+            .getElementById("hours")
+            .innerHTML = "<p>" + hours + " hours</p>";
+        document
+            .getElementById("minutes")
+            .innerHTML = "<p>" + minutes + " minutes</p>";
+        document
+            .getElementById("seconds")
+            .innerHTML = "<p>" + seconds + " seconds</p>";
 
-    // If the count down is finished, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countDown").innerHTML = "WE GOT HITCHED!";
-    }
-}, 1000);
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document
+                .getElementById("countDown")
+                .innerHTML = "WE GOT HITCHED!";
+        }
+    }, 1000);
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,63 +1,96 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("collapse").style.marginLeft = "250px";
+    document
+        .getElementById("mySidenav")
+        .style
+        .width = "250px";
+    document
+        .getElementById("collapse")
+        .style
+        .marginLeft = "250px";
+    document
+        .getElementById("collapse2")
+        .style
+        .marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("collapse").style.marginLeft = "0";
+    document
+        .getElementById("mySidenav")
+        .style
+        .width = "0";
+    document
+        .getElementById("collapse")
+        .style
+        .marginLeft = "0";
+    document
+        .getElementById("collapse2")
+        .style
+        .marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
 
+var navListItems = $('div.setup-panel-3 div a'),
+    allWells = $('.setup-content-3'),
+    allNextBtn = $('.nextBtn-3'),
+    allPrevBtn = $('.prevBtn-3');
 
+allWells.hide();
 
+navListItems.click(function (e) {
+    e.preventDefault();
+    var $target = $($(this).attr('href')),
+        $item = $(this);
 
+    if (!$item.hasClass('disabled')) {
+        navListItems
+            .removeClass('btn-info')
+            .addClass('btn-pink');
+        $item.addClass('btn-info');
+        allWells.hide();
+        $target.show();
+        $target
+            .find('input:eq(0)')
+            .focus();
+    }
+});
 
-//map
-function initMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(32.029, -81.06),
-        zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    var uluruC = {
-        lat: 31.965976,
-        lng: -81.068378
-    };
-    var uluruR = {
-        lat: 32.019672,
-        lng: -81.016457
-    };
-    var cInfo = "<div id='cInfo'><h4>Wormsloe Plantation</h3>";
+allPrevBtn.click(function () {
+    var curStep = $(this).closest(".setup-content-3"),
+        curStepBtn = curStep.attr("id"),
+        prevStepSteps = $('div.setup-panel-3 div a[href="#' + curStepBtn + '"]')
+            .parent()
+            .prev()
+            .children("a");
 
-    var rInfo = "<h4 id='rInfo'>Savannah Yacht Club</h3>"
+    prevStepSteps
+        .removeAttr('disabled')
+        .trigger('click');
+});
 
-    var cInfowindow = new google.maps.InfoWindow({
-        content: cInfo,
-        maxWidth: 200
-    });
-    var rInfowindow = new google.maps.InfoWindow({
-        content: rInfo,
-        maxWidth: 200
-    });
+allNextBtn.click(function () {
+    var curStep = $(this).closest(".setup-content-3"),
+        curStepBtn = curStep.attr("id"),
+        nextStepSteps = $('div.setup-panel-3 div a[href="#' + curStepBtn + '"]')
+            .parent()
+            .next()
+            .children("a"),
+        curInputs = curStep.find("input[type='text'],input[type='url']"),
+        isValid = true;
 
-    var ceremonyMarker = new google.maps.Marker({
-        position: uluruC,
-        map: map,
-        title: 'Reception'
-    });
-    var receptionMarker = new google.maps.Marker({
-        position: uluruR,
-        map: map,
-        title: "Ceremony"
-    });
+    $(".form-group").removeClass("has-error");
+    for (var i = 0; i < curInputs.length; i++) {
+        if (!curInputs[i].validity.valid) {
+            isValid = false;
+            $(curInputs[i])
+                .closest(".form-group")
+                .addClass("has-error");
+        }
+    }
 
-    ceremonyMarker.addListener('click', function () {
-        cInfowindow.open(map, ceremonyMarker);
-    });
-    receptionMarker.addListener('click', function () {
-        rInfowindow.open(map, receptionMarker);
-    });
-}
+    if (isValid) 
+        nextStepSteps.removeAttr('disabled').trigger('click');
+    }
+);
+
+$('div.setup-panel-3 div a.btn-info').trigger('click');
